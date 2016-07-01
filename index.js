7785794f9baa24c92a4d21de45a925f05cf8422a
Elijah Prince <elijahprince73@gmail.com>
2016-06-30 17:56:48 -0700

    Changing the box color and square

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-document.getElementById("guess").onclick = function () {
-	var gotIt=false;
-	var guesses=1;
-	while (gotIt==false) {
+var clickedTime;
+var createdTime;
+var reactionTime;
 
-		var x = Math.random();
-
-		x = 6 * x;
-
-		x = Math.floor(x);
-
-		if (document.getElementById("myNumber").value == x) {
-			gotIt=true
-		} else {
-guesses++;
-		}
+function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
 	}
+	return color;
+}
 
+alert(getRandomColor());
+
+function makeBox() {
+
+	createdTime = Date.now();
+
+	var time = Math.random();
+
+	time = time * 4000;
+	setTimeout(function () {
+
+		if (Math.random() > 0.5) {
+			document.getElementById("box").style.borderRadius = "100px";
+		} else {
+			document.getElementById("box").style.borderRadius = "0px";
+		}
+		
+		var top=Math.random();
+		
+		top=top*300;
+		
+		var left=Math.random();
+		
+		left=left*500;
+		
+		document.getElementById("box").style.top=top+"px";
+		document.getElementById("box").style.left=left+"px";
+		document.getElementById("box").style.backgroundColor = getRandomColor();
+
+		document.getElementById("box").style.display = "block";
+
+		createdTime = Date.now();
+	}, time);
+}
+
+document.getElementById("box").onclick = function () {
+
+	clickedTime = Date.now();
+
+	reactionTime = (clickedTime - createdTime) / 1000;
+
+	document.getElementById("time").innerHTML = reactionTime;
+
+	this.style.display = "none";
+
+	makeBox();
 };
+
+makeBox();
