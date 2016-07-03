dcfe81c2f47dccad64f0659c4ea1c591fb515f51
Elijah Prince <elijahprince73@gmail.com>
2016-07-02 18:31:16 -0700

    added new animations

--- jquery/index.js
+++ jquery/index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+$("#circle").click(function () {
+
+$(this).animate({
+	height:"300px",
+	borderRadius:"150px",
+	marginLeft:"100px",
+	marginTop:"100px",
+	width:"300px"},1000);
+
+});
