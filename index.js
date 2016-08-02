7ef3cac3c659c583c0163b054bba719d59b1108e
Elijah Prince <elijahprince73@gmail.com>
2016-08-02 01:12:59 -0700

    added new characters to array

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-var myArray = ["superman ", "batman " , "hulk"];
+var myArray = ["superman ", "batman ", "hulk"];
+
+
+
+var character = document.getElementById("character").value;
+
+document.getElementById("addCharacter").onclick = function () {
+	
+character = document.getElementById("character").value;
+	myArray.push(character)
+	console.log(myArray);
+
+	document.getElementById("square").innerHTML= "New Array: " + myArray;
+}
+
+
 
 //document.write(myArray);
 //document.write(myArray[0]);
@@ -9,11 +24,16 @@ var myArray = ["superman ", "batman " , "hulk"];
 //document.getElementById("square").innerHTML = myArray;
 
 //myArray.pop()
-	//document.getElementById("square").innerHTML = myArray;
-	//myArray.shift();
-	//document.getElementById("square").innerHTML = myArray;
+//document.getElementById("square").innerHTML = myArray;
+//myArray.shift();
+//document.getElementById("square").innerHTML = myArray;
 //myArray.unshift("Iron Man");
 //document.getElementById("square").innerHTML = myArray;
 
-delete myArray[0];
-document.getElementById("square").innerHTML = myArray;
+//delete myArray[0];
+//document.getElementById("square").innerHTML = myArray;
+//myArray.splice(0,2);
+//document.getElementById("square").innerHTML = myArray;
+
+//myArray.splice(1,0 ,"Ant-Man");
+//document.getElementById("square").innerHTML = myArray;
