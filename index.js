971e537e33a269e00e84fd722a5bda294ca8ba1f
Elijah Prince <elijahprince73@gmail.com>
2016-08-02 00:56:25 -0700

    added javascript functions

---
+++ index.js
@@ -0,0 +1,19 @@
+var myArray = ["superman ", "batman " , "hulk"];
+
+//document.write(myArray);
+//document.write(myArray[0]);
+
+//document.getElementById("square").innerHTML = myArray;
+
+//myArray.push("Captain-America");
+//document.getElementById("square").innerHTML = myArray;
+
+//myArray.pop()
+	//document.getElementById("square").innerHTML = myArray;
+	//myArray.shift();
+	//document.getElementById("square").innerHTML = myArray;
+//myArray.unshift("Iron Man");
+//document.getElementById("square").innerHTML = myArray;
+
+delete myArray[0];
+document.getElementById("square").innerHTML = myArray;
