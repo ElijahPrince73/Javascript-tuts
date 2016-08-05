0cc897ff7a2c0b319264d2f92526c7da17082959
Elijah Prince <elijahprince73@gmail.com>
2016-08-04 17:27:05 -0700

    finished tabs with jquery

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
-//var testInterval = setInterval(function(){
+$(function() { 
 	
-	//alert("this is my interval alert")
-//},
-
-var myInterval = setInterval(function(){
+	  $(".tab-panels .tabs li").on('click',function() { 
+	  
+		var panelToDisplay = $(this).attr('rel');
+		
+		$('.tab-panels .tabs').find('li.active').removeClass('active');
+		$(this).addClass('active');
+		
+		$('.tab-panels .panel.active').slideUp(300,function() { 
+		
+		  $(this).removeClass('active');
+		  $('#'+panelToDisplay).slideDown(300,function() { 
+		    $(this).addClass('active');
+		  });
+		
+		
+		});
+	  
+	  });
 	
-	timer()
-},1000);
-
-var myButton = document.getElementsByTagName("button")[0];
-
-
-function timer (){
-	
-	var theDate = new Date();
-	
-	var theTime = theDate.toTimeString();
-	document.getElementById("showTimer").innerHTML = theTime;
-}
-
-function stopTimer (){
-	clearInterval(myInterval);
-	myButton.innerHTML = "Start Time";
-	myButton.setAttribute("onclick" , "startTime()");
-}
-
-function startTime(){
-	myInterval = setInterval(function(){
-		timer()
-	}, 1000);
-	
-	myButton.innerHTML = "stop Time";
-	myButton.setAttribute("onclick" , "stopTimer()");
-	
-}
+	});
